327fcd47397bac97e3876e931b467890708cfc9d
artemgassan <artemgassan@ya.ru>
2024-07-21 13:06:53 +0300

    feat: connect react

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,17 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+    const cssLoader =  {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
 
     const typescriptLoader = {
         test: /\.tsx?$/,
@@ -10,5 +21,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
