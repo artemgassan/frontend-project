1cff0d53fc3b7d2a21e76385cf6f2a04a7a5b800
artemgassan <artemgassan@ya.ru>
2024-07-26 16:20:54 +0300

    feat: created a configuration for routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,20 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
-
 
     return (
         <div className={classNames('app', {hovered: true, selected: false}, [theme, 'cls2', 'cls3'])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>} />
-                    <Route path={'/'} element={<MainPage/>} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
