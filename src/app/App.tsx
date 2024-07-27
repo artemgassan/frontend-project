70d12076146b094088b21eabc036823b5a53438a
artemgassan <artemgassan@ya.ru>
2024-07-27 05:09:18 +0300

    feat: the navbar is implemented

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './styles/index.scss';
-import { Link } from 'react-router-dom';
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={classNames('app', {hovered: true, selected: false}, [theme, 'cls2', 'cls3'])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
