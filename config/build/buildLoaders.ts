c345f1c8c89a8209ca1d587ec218221a27fcf6e3
artemgassan <artemgassan@ya.ru>
2024-07-27 10:10:24 +0300

    feat: added svg and file loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,12 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
     const cssLoader =  {
         test: /\.s[ac]ss$/i,
         use: [
@@ -28,8 +34,20 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const fileLoader = {
+        test: /\.(png|jp?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {}
+            }
+        ]
+    }
+
     return [
         typescriptLoader,
         cssLoader,
+        svgLoader,
+        fileLoader,
     ]
 }
