075c50ea5580c715c4bd0f95203bb1019f4b3e1e
artemgassan <artemgassan@ya.ru>
2024-08-16 14:30:14 +0300

    feat: connection storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testing'],
+                ignoreAttribute: ['data-testing', 'to'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
