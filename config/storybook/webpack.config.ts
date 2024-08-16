8639ed46bac8be1b42619aea1c6c6a7de405dd98
artemgassan <artemgassan@ya.ru>
2024-08-16 16:23:20 +0300

    feat: add stories for sidebar

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
@@ -13,6 +13,17 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
 
+    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+        if(/svg/.test(rule.test as string)) {
+            return {...rule, exclude: /\.svg$/i};
+        }
+        return rule;
+    })
+
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
     config.module.rules.push(buildCssLoader(true));
 
     return config;
