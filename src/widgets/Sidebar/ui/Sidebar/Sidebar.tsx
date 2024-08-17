84676766c16dbba6e76449fffcac0ca642558f00
artemgassan <artemgassan@ya.ru>
2024-08-17 17:42:48 +0300

    refactor: fix lint errors

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,11 +4,11 @@ import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-import cls from './Sidebar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
     className?: string;
@@ -44,7 +44,7 @@ const Sidebar = ({ className }: SidebarProps) => {
                         to={RoutePath.main}
                         className={cls.item}
                     >
-                        <MainIcon className={cls.icon}/>
+                        <MainIcon className={cls.icon} />
                         <span className={cls.link}>{t('Главная')}</span>
                     </AppLink>
                 </div>
@@ -54,7 +54,7 @@ const Sidebar = ({ className }: SidebarProps) => {
                         to={RoutePath.about}
                         className={cls.item}
                     >
-                        <AboutIcon className={cls.icon}/>
+                        <AboutIcon className={cls.icon} />
                         <span className={cls.link}>{t('О сайте')}</span>
                     </AppLink>
                 </div>
