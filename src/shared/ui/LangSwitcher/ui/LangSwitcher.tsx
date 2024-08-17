4b3cd920cacf4fbed270db73d76b2bd7af8f10a3
artemgassan <artemgassan@ya.ru>
2024-08-17 13:58:32 +0300

    feat: change style button component

--- src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,6 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface LangSwitcherProps {
     className?: string;
@@ -16,7 +16,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
     return (
         <Button
             className={classNames('', {}, [className])}
-            theme={ThemeButton.CLEAR}
+            theme={ButtonTheme.CLEAR}
             onClick={toggle}
         >
             {t('Язык')}
