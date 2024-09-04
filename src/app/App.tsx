169c7dffee4dbecd958025e341ff28487d326b46
artemgassan <artemgassan@ya.ru>
2024-09-04 23:05:47 +0300

    feat: add a basic modal window template

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,22 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Suspense, useState } from 'react';
 
 const App = () => {
     const { theme } = useTheme();
+
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    random text dsklfjsfjs sdlfjskldf sdflksdnfl dfskdgslkjf sfleknfwl efkekldsjf ldkfsdlkfjsdf  sdfjskdf sdfksjdfnkjsd fksdjfnskd fksdjfnkjsdfs dfkjsndkfjsfnds dkfjsdnfksjnf
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,46 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import React, { ReactNode } from 'react';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+}
+
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+    } = props;
+
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen,
+    };
+
+    const onContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    const closeHandler = () => {
+        if (onClose) {
+            onClose();
+        }
+    };
+
+    return (
+        <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={cls.overlay} onClick={closeHandler}>
+                <div
+                    className={cls.content}
+                    onClick={onContentClick}
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
