1697512e21fa0952c88503b4b4dd3dccede59697
artemgassan <artemgassan@ya.ru>
2024-09-05 11:48:18 +0300

    fix: fixed animation close modal window

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,5 +1,7 @@
-import { classNames } from "shared/lib/classNames/classNames";
-import React, { ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import React, {
+    ReactNode, useCallback, useEffect, useRef, useState,
+} from 'react';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
@@ -9,6 +11,8 @@ interface ModalProps {
     onClose?: () => void;
 }
 
+const ANIMATION_DELAY = 300;
+
 export const Modal = (props: ModalProps) => {
     const {
         className,
@@ -17,19 +21,44 @@ export const Modal = (props: ModalProps) => {
         onClose,
     } = props;
 
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
     };
+
+    const closeHandler = () => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
+        }
+    };
+
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeHandler();
+        }
+    }, [closeHandler]);
 
     const onContentClick = (e: React.MouseEvent) => {
         e.stopPropagation();
     };
 
-    const closeHandler = () => {
-        if (onClose) {
-            onClose();
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
         }
-    };
+
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen]);
 
     return (
         <div className={classNames(cls.Modal, mods, [className])}>
