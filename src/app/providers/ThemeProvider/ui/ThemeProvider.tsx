af8164cadc8b0d9069a79cf6fa8d54e550d655ed
artemgassan <artemgassan@ya.ru>
2024-09-05 13:11:27 +0300

    feat: implemented modal window

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,10 +1,19 @@
 import React, { FC, useMemo, useState } from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
-const ThemeProvider: FC = ({ children }) => {
+interface ThemeProviderProps {
+    initialTheme?: Theme,
+}
+
+const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+    const {
+        initialTheme,
+        children,
+    } = props;
+
     const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-    const [theme, setTheme] = useState<Theme>(defaultTheme);
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
     const defaultProps = useMemo(() => ({
         theme,
