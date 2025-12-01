0292c1a1e63be7e0f91aed9d86db79b80f089db2
artemgassan <artemgassan@ya.ru>
2025-12-02 00:52:59 +0300

    feat: set up redux store

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { createReduxStore } from '../config/store';
+import { StateSchema } from '../config/StateSchema';
 
 interface StoreProviderProps {
     children?: ReactNode;
+    initialState?: StateSchema;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
     const {
         children,
+        initialState,
     } = props;
 
-    const store = createReduxStore();
+    const store = createReduxStore(initialState);
 
     return (
         <Provider store={store}>
