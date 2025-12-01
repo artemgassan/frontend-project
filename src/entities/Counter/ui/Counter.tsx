e92605299ef58fa67e117ea43cbb6363701b6d3c
artemgassan <artemgassan@ya.ru>
2025-12-02 02:06:00 +0300

    feat: set up redux store

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,32 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { counterActions } from '../model/slice/counterSlice';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue);
+
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+
+    return (
+        <div>
+            <h1>
+                value =
+                {counterValue}
+            </h1>
+            <Button onClick={increment}>
+                increment
+            </Button>
+            <Button onClick={decrement}>
+                decrement
+            </Button>
+        </div>
+    );
+};
