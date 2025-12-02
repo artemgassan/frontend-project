b306a59f011d0ceba84bf767ae91bd1282d9cc5b
artemgassan <artemgassan@ya.ru>
2025-12-02 13:58:58 +0300

    refactor: changed global theme variables

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -30,7 +30,6 @@ export const Modal = (props: ModalProps) => {
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
 
     const closeHandler = useCallback(() => {
